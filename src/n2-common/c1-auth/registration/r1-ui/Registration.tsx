1cd346445f95f667129a0366067c5c6bc9591613
Talman95 <taltro528@gmail.com>
2022-05-29 19:31:56 +0300

    added show loading after request

--- src/n2-common/c1-auth/registration/r1-ui/Registration.tsx
+++ src/n2-common/c1-auth/registration/r1-ui/Registration.tsx
@@ -1,5 +1,5 @@
 import React, {ChangeEvent, FC} from 'react'
-import { NavLink } from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 import s from './Registration.module.css'
 
 type RegistrationPropsType = {
@@ -23,42 +23,46 @@ export const Registration: FC<RegistrationPropsType> = (
     }
 ) => {
     return (
-        <div className={s.container}>
-            <h1>it-incubator</h1>
-            <h3>Sign Up</h3>
-            <input
-                name={"email"}
-                type={"text"}
-                value={email}
-                onChange={setEmail}
-                placeholder={"Enter your email..."}
-            />
-            <input
-                name={"password"}
-                type={"text"}
-                value={password}
-                onChange={setPassword}
-                placeholder={"Enter your password..."}
-            />
-            <input name={"password2"}
-                   type={"text"}
-                   value={password2}
-                   onChange={setPassword2}
-                   placeholder={"Confirm your password..."}
-            />
-            <div className={s.buttonsBlock}>
-                <NavLink to="/login">
-                    Cancel
-                </NavLink>
-                <button
-                    type={"submit"}
-                    onClick={signUp}
-                    disabled={loading}
-                >
-                    Register
-                </button>
+        loading
+            ?
+            <div>Loading ...</div>
+            :
+            <div className={s.container}>
+                <h1>it-incubator</h1>
+                <h3>Sign Up</h3>
+                <input
+                    name={"email"}
+                    type={"text"}
+                    value={email}
+                    onChange={setEmail}
+                    placeholder={"Enter your email..."}
+                />
+                <input
+                    name={"password"}
+                    type={"text"}
+                    value={password}
+                    onChange={setPassword}
+                    placeholder={"Enter your password..."}
+                />
+                <input name={"password2"}
+                       type={"text"}
+                       value={password2}
+                       onChange={setPassword2}
+                       placeholder={"Confirm your password..."}
+                />
+                <div className={s.buttonsBlock}>
+                    <NavLink to="/login">
+                        Cancel
+                    </NavLink>
+                    <button
+                        type={"submit"}
+                        onClick={signUp}
+                        disabled={loading}
+                    >
+                        Register
+                    </button>
+                </div>
+                <div>{error}</div>
             </div>
-            <div>{error}</div>
-        </div>
     )
 };
