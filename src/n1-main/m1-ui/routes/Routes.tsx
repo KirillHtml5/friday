dcb5d588eca84e6eeda371d30c1ffc2a5bd0c049
DimaDeg <ddeg1809@gmail.com>
2022-06-02 19:02:59 +0300

    upadate

--- src/n1-main/m1-ui/routes/Routes.tsx
+++ src/n1-main/m1-ui/routes/Routes.tsx
@@ -1,17 +1,18 @@
 import {Navigate, Route, Routes} from "react-router-dom"
 import RecoveryPassword from "../../../n2-common/c1-auth/RecoveryPassword";
-import Registration from "../../../n2-common/c1-auth/Registration";
 import Test from "../../../n2-common/Test";
 import Error404 from "../../../n2-common/Error404";
-import Profile from "../../../n2-common/c1-auth/Profile";
 import {LoginContainer} from "../../../n2-common/c1-auth/login/LoginContainer";
 import {NewPasswordContainer} from "../../../n2-common/c1-auth/NewPassword/NewPasswordContainer";
+import Profile from "../../../n2-common/kirill/k1-ui/Profile";
+import {RegistrationContainer} from "../../../n2-common/c1-auth/registration/r1-ui/RegistrationContainer";
+
 
 const MyRoutes = () => {
     return <div>
         <Routes>
             <Route path={'login'} element={<LoginContainer/>}/>
-            <Route path={'registration'} element={<Registration/>}/>
+            <Route path={'registration'} element={<RegistrationContainer/>}/>
             <Route path={'/'} element={<Profile/>}/>
             <Route path={'/404'} element={<Error404/>}/>
             <Route path={'recovery-password'} element={<RecoveryPassword/>}/>
