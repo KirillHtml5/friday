64004ff30f31d6ca10ed0489d89d425438c0fbf3
DimaDeg <ddeg1809@gmail.com>
2022-06-14 20:53:11 +0300

    cards updates

---
+++ src/n1-main/m1-ui/common/c5-SuperSelect/SuperSelect.tsx
@@ -0,0 +1,34 @@
+import React, {SelectHTMLAttributes, DetailedHTMLProps, ChangeEvent} from 'react'
+import  s from './SuperSelect.module.css'
+
+type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
+
+type SuperSelectPropsType = DefaultSelectPropsType & {
+    options?: any[]
+    onChangeOption?: (option: any) => void
+}
+
+const SuperSelect: React.FC<SuperSelectPropsType> = (
+    {
+        options,
+        onChange, onChangeOption,
+        ...restProps
+    }
+) => {
+    const mappedOptions: any[] = options ? options.map((f,i)=>(
+            <option key={i} value={f}>{f}</option>
+            )):[]; // map options with key
+
+    const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
+        onChange && onChange(e)
+        onChangeOption && onChangeOption(e.currentTarget.value)
+    }
+
+    return (
+        <select className={s.select} onChange={onChangeCallback} {...restProps}>
+            {mappedOptions}
+        </select>
+    )
+}
+
+export default SuperSelect
