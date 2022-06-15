cfd2d66a52e79f75c1d97b5af1027f57767abf09
Talman95 <taltro528@gmail.com>
2022-06-15 22:48:35 +0300

    added modal to delete cards

--- src/n2-common/c2-cards/cards/x1-table/cardsItems/cardsItems.tsx
+++ src/n2-common/c2-cards/cards/x1-table/cardsItems/cardsItems.tsx
@@ -1,8 +1,9 @@
-import React, {FC} from 'react';
-import {changeCardsPerPage, deleteCard, updateCard} from "../../../../../n1-main/m2-bll/reducers/Cards-reducer";
+import React, {FC, useState} from 'react';
+import {updateCard} from "../../../../../n1-main/m2-bll/reducers/Cards-reducer";
 import {useDispatch, useSelector} from "react-redux";
 import {ReduxRootType} from "../../../../../n1-main/m2-bll/store/ReduxStore";
 import s from '../../../packs/p1-ui/packs/PacksTable.module.css'
+import {DeleteCardModal} from "../../x3-cardsModal/c2-deleteCardModal/DeleteCardModal";
 
 type CardsItemsType = {
     question: string
@@ -18,17 +19,16 @@ export const CardsItem: FC<CardsItemsType> = (props) => {
 
     const dispatch = useDispatch<any>()
 
+    const [showDeleteCardModal, setShowDeleteCardModal] = useState<boolean>(false)
     const myId = useSelector<ReduxRootType, string>(state => state.profile.user._id)
-    const pageCount = useSelector<ReduxRootType,number>(state => state.cards.pageCount)
 
     const {question, answer, grade, updated, cardsPack_id, id, user_id} = props
 
-    const deleteSelectCard = () => {
-        dispatch(changeCardsPerPage(pageCount))
-    }
-
     const editSelectCard = () => {
         dispatch(updateCard(cardsPack_id, id))
+    }
+    const onDeleteModalHandler = () => {
+        setShowDeleteCardModal(true)
     }
 
     return (
@@ -42,10 +42,18 @@ export const CardsItem: FC<CardsItemsType> = (props) => {
                 {user_id ===  myId ?
                     <>
                         <button onClick={editSelectCard}>Edit</button>
-                        <button onClick={deleteSelectCard}>Delete</button>
+                        <button onClick={onDeleteModalHandler}>Delete</button>
                     </> : ''
                 }
             </td>
+            {showDeleteCardModal &&
+                <DeleteCardModal
+                setShowModal={setShowDeleteCardModal}
+                name={question}
+                pack_id={cardsPack_id}
+                cardId={id}
+                />
+            }
         </tr>
     );
 };
