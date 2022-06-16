f68bb8f6163d001ddfffafbd90ca352fc313fe30
Talman95 <taltro528@gmail.com>
2022-06-16 22:14:45 +0300

    added all

--- src/n2-common/c2-cards/cards/x2-cards-page/cardsPage.tsx
+++ src/n2-common/c2-cards/cards/x2-cards-page/cardsPage.tsx
@@ -2,14 +2,11 @@ import {CardsTable} from "../x1-table/CardsTable";
 import {useDispatch, useSelector} from "react-redux";
 import {ReduxRootType} from "../../../../n1-main/m2-bll/store/ReduxStore";
 import {
-    addCard,
     CardsStateType, changeCardsPerPage,
     getCards,
     ThunkDispatchActionType, changeSearchBy
 } from "../../../../n1-main/m2-bll/reducers/Cards-reducer";
 import React, {ChangeEvent, useEffect, useState} from "react";
-import {useNavigate, useParams} from "react-router-dom";
-import {ChangeEvent, useEffect, useState} from "react";
 import {NavLink, useNavigate, useParams} from "react-router-dom";
 import c from '../../../c1-auth/loading/loading.module.css';
 import s from './cardsPage.module.css';
@@ -55,8 +52,7 @@ export const CardsPage = () => {
         } else
             id && dispatch(getCards(id))
         dispatch(changeSearchBy(debounceQuestion, debounceAnswer))
-    }, [isLoggedIn, navigate,dispatch,cardsTotalCount, id, sortCards, cardAnswer,pageCount,page, cardQuestion, debounceAnswer, debounceQuestion])
-
+    }, [isLoggedIn, navigate, dispatch, cardsTotalCount, id, sortCards, cardAnswer, pageCount, page, cardQuestion, debounceAnswer, debounceQuestion])
 
 
     const changePageSize = (e: ChangeEvent<HTMLSelectElement>) => {
@@ -68,49 +64,17 @@ export const CardsPage = () => {
     }
 
     return (isLoad ?
-        <div className={c.preloader}/> :
-        <div>
-            <NavLink to={'/packs'}>
-                <span className={s.return}>Packs</span>
-            </NavLink>
-            <h2>{namePack}</h2>
-            <div className={s.inputs}>
-                <SuperInputText value={changeQuestion} onChangeText={setChangeQuestion}
-                                placeholder={'question search'}/>
-                <SuperInputText value={changeAnswer} onChangeText={setChangeAnswer} placeholder={'answer search'}/>
-            </div>
-            <div className={s.searchBlock}>
-                <div className={s.inputBlock}>
-                    <SuperInputText value={changeQuestion} onChangeText={setChangeQuestion}
-                                    placeholder={'question search'} className={s.input}/>
-                    <SuperButton onClick={searchByQuestion}>Search Question</SuperButton>
-                </div>
-                <div className={s.inputBlock}>
-                    <SuperInputText value={changeAnswer} onChangeText={setChangeAnswer}
-                                    placeholder={'answer search'} className={s.input}/>
-                    <SuperButton onClick={searchByAnswer}>Search Answer</SuperButton>
-                </div>
-            </div>
-            <CardsTable/>
-            <div className={s.button}>
-                {userId === packUserId ? <button onClick={addNewCard}>Add Card</button> : ""}
-            </div>
-            <CardsPagination/>
-            <SuperSelect value={newPageCount} options={selectRatio} onChange={changePageSize}/>
-
-        </div>)
+            <div className={c.preloader}/> :
+            <div>
+                <NavLink to={'/packs'}>
+                    <span className={s.return}>Packs</span>
+                </NavLink>
                 <h2>{namePack}</h2>
                 <div className={s.searchBlock}>
-                    <div className={s.inputBlock}>
-                        <SuperInputText value={changeQuestion} onChangeText={setChangeQuestion}
-                                        placeholder={'question search'} className={s.input}/>
-                        <SuperButton onClick={searchByQuestion}>Search Question</SuperButton>
-                    </div>
-                    <div className={s.inputBlock}>
-                        <SuperInputText value={changeAnswer} onChangeText={setChangeAnswer}
-                                        placeholder={'answer search'} className={s.input}/>
-                        <SuperButton onClick={searchByAnswer}>Search Answer</SuperButton>
-                    </div>
+                    <SuperInputText value={changeQuestion} onChangeText={setChangeQuestion}
+                                    placeholder={'question search'} className={s.input}/>
+                    <SuperInputText value={changeAnswer} onChangeText={setChangeAnswer}
+                                    placeholder={'answer search'} className={s.input}/>
                 </div>
                 <CardsTable/>
                 {userId === packUserId &&
@@ -120,8 +84,9 @@ export const CardsPage = () => {
                 }
                 <CardsPagination/>
                 <SuperSelect value={newPageCount} options={selectRatio} onChange={changePageSize}/>
-                {showAddCardModal && <AddCardModal setShowModal={setShowAddCardModal} packId={packId}/>}
-            </>
+                {showAddCardModal && <AddCardModal setShowModal={setShowAddCardModal} packId={id}/>}
+
+            </div>
     )
 }
 
